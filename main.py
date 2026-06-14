fe675b8542b75119720ddca10baef8127e772394
thomas-j-vincent <thomasjvincent@outlook.com>
2026-06-14 01:08:21 +0100

    it works!!!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #new project!!
 import csv
-import matplotlib 
+import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
@@ -15,11 +16,13 @@ colour = "g"
 dates = []
 entries = []
 prices = []
+inputDate = input("Enter the date you want to find a price for(MM/DD/YY):")
+#inputDate = "10/10/24"
 
 
 with open(filePath, "r") as file:
     #lines = file.readlines()
-    #print (lines)
+    #print (lines)10
     # for line in lines[1:]:
     reader = csv.reader(file, delimiter=",")
     next(reader)
@@ -28,34 +31,44 @@ with open(filePath, "r") as file:
         dates.append(date)
         price = (line[1])
         prices.append(float(price))
-        print(date)
+        #print(date)
 
 for entry in range (len(dates)):
     entries.append(entry)
     #print(entries)
 
-#dates = [datetime.strptime(d, "%m/%d/%y") for d in dates]
+dates = [datetime.strptime(d, "%m/%d/%y") for d in dates]
+inputDate = [datetime.strptime(inputDate, "%m/%d/%y")]
+#print(type(dates))
+#print(dates)
 
-slope, intercept, r, p, std_err = stats.linregress(entries, prices)
+dates_numeric = np.array([d.timestamp() for d in dates])
+inputDate_numeric = np.array([d.timestamp() for d in inputDate])
+#slope, intercept, r, p, std_err = stats.linregress(entries, prices)
+slope, intercept, r, p, std_err = stats.linregress(dates_numeric, prices)
 
 def myfunc(x):
   return slope * x + intercept
 
-mymodel = list(map(myfunc, entries))
+#mymodel = list(map(myfunc, entries))
+mymodel = list(map(myfunc, dates_numeric))
 
-speed = myfunc(100)
+result = myfunc(inputDate_numeric)
 
-print(speed)
+print(result)
 
-ax = plt.gca()
+#ax = plt.gca()
 
-ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+#ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+#ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
 
-plt.xticks(rotation=45)
+#plt.xticks(rotation=45)
 
-plt.scatter(dates, prices, color= colour)
-plt.plot(entries, mymodel)
-plt.ylabel("Account Value")
-plt.xlabel("wager Count")
+plt.scatter(dates_numeric, prices, color= colour)
+#plt.plot(entries, mymodel)
+plt.plot(dates_numeric, mymodel)
+plt.scatter(inputDate_numeric[0], result[0], color='red', zorder=5, label=f'Prediction: {result[0]:.2f}')
+plt.ylabel("prices")
+plt.xlabel("dates numeric")
+plt.legend()
 plt.show()
